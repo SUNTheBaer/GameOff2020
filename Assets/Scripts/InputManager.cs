e00cc7882f827822d1262188b0b8d7869a6370f4
SUNTheBaer <padron1999@yahoo.com>
2020-11-02 04:29:35 -0500

    Inputs, player movement, camera setup

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    private Inputs inputs;
+    [HideInInspector] public Vector2 move;
+    private bool pressed;
+    
+    private void Awake()
+    {
+        inputs = new Inputs();
+
+        inputs.Player.Movement.performed += context => move = context.ReadValue<Vector2>();
+        inputs.Player.Movement.canceled += context => move = Vector2.zero;
+    }
+
+    private void OnEnable()
+    {
+        inputs.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputs.Player.Disable();
+    }
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    //Scripts
+    public InputManager inputManager;
+    public PlayerMovement playerMovement;
+    public PlayerCollision playerCollision;
+
+    //Components
+    [HideInInspector] public Rigidbody2D rb;
+    private Animator anim;
+
+    //Player attributes
+    public float speed;
+
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        
+    }
+}
