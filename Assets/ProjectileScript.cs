fdd2e9bafc1f377e86c09faea7066ae1fdb47445
David Paez <dpaez013@fiu.edu>
2020-11-03 03:01:59 -0500

    Created scene and projectile-based scripts

---
+++ Assets/ProjectileScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileScript : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        rb2d.AddForce(new Vector2(2,0));
+    }
+}
---
+++ Assets/Scripts/ShootProjectile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootProjectile : MonoBehaviour
+{
+    Animator animator;
+    Object projectileRef;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        projectileRef = Resources.Load("Basic_Projectile");
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            animator.Play("Zee");
+            GameObject projectile = (GameObject)Instantiate(projectileRef);
+            projectile.transform.position = new Vector3(transform.position.x + .4f, transform.position.y + 2f, -1);
+        }   
+    }
+}
