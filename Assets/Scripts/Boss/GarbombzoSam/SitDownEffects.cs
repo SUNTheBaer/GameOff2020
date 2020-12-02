782b503689eeb4e03d058ca73f6089aa85904408
Loc-19 <freiref2012@gmail.com>
2020-12-02 15:51:19 -0500

    I really hope I'm doing this right

---
+++ Assets/Scripts/Boss/GarbombzoSam/SitDownEffects.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class SitDownEffects : MonoBehaviour
+{
+    [SerializeField] CinemachineVirtualCamera explodeShake = null;
+    [SerializeField] int camPriority = 0;
+    [SerializeField] bool play = false;
+
+    private void Update()
+    {
+        if (play)
+            explodeShake.Priority = camPriority;
+    }
+}
